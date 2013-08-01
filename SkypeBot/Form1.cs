b4667d02ae411fa411571d24a30c466dafae2003
Enzo <enzodayrit11@gmail.com>
2013-08-01 15:51:47 +0900

    First commit

---
+++ SkypeBot/Form1.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Windows.Forms;
+using SKYPE4COMLib; // Our COM library
+using System.Collections.Generic;
+
+namespace SkypeBot
+{
+    public partial class Form1 : Form
+    {
+        private Skype skype;
+        private const string trigger = "!"; // Say !help
+        private const string nick = "BOT";
+        private Dictionary<string, Game> gameDic;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            gameDic = new Dictionary<string, Game>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            skype = new Skype();
+            // Use skype protocol version 7 
+            skype.Attach(7, false);
+            // Listen 
+            skype.MessageStatus +=
+              new _ISkypeEvents_MessageStatusEventHandler(skype_MessageStatus);
+        }
+        private void skype_MessageStatus(ChatMessage msg,
+                     TChatMessageStatus status)
+        {
+            Console.WriteLine(status);
+            if (status.ToString() != "cmsReceived" && status.ToString() != "cmsSending")
+            {
+                return;
+            }
+
+            
+
+            richTextBox1.AppendText(msg.FromDisplayName+": "+msg.Body + "\n");
+            // Proceed only if the incoming message is a trigger
+            if (msg.Body.IndexOf(trigger) == 0 )
+            {
+                // Remove trigger string and make lower case
+                string command = msg.Body.Remove(0, trigger.Length).ToLower();
+                
+                // Send processed message back to skype chat window
+                IChat ichat = skype.get_Chat(msg.Chat.Name);
+
+                String msg2 = ProcessCommand(command, ichat, msg);
+
+                if(msg2.Length > 0)
+                    ichat.SendMessage(nick + ": " + msg2);
+
+
+                msg.Seen = true;
+            }
+
+        }
+
+        public void say(String msg, IChat ichat, String cat = "")
+        {
+            if (cat.Length > 0)
+            {
+                cat = "[" + cat + "]";
+            }
+
+            ichat.SendMessage(nick + cat + ": " + msg);
+        }
+
+        private string ProcessCommand(string str, IChat ichat, ChatMessage msg)
+        {
+            string result;
+            switch (str.Split(' ')[0])
+            {
+                case "roll":
+                    try
+                    {
+                        /*String[] a = str.Split(' ', '+');
+                        String[] b = a[1].Split('d');
+                        result = RollDice(Int32.Parse(b[0]),Int32.Parse(b[1]));*/
+                        result = parseDice(str);
+                    }
+                    catch (Exception e)
+                    {
+                        result = "nice try";
+                    }
+                    break;
+                case "attack":
+
+                    result = parseAttack(str, ichat, msg);
+
+                    break;
+                default:
+                    result = "";
+                    break;
+            }
+
+            return result;
+        }
+
+        private String parseAttack(string str, IChat ichat, ChatMessage msg)
+        {
+            if (!gameDic.ContainsKey(ichat.Topic))
+            {
+                gameDic[ichat.Topic] = new Game(this, ichat);
+            }
+
+            string[] strs = str.Split(' ');
+
+            if (strs.Length > 1)
+            {
+
+                User target = findPlayer(strs[1], ichat);
+
+                if (target == null)
+                {
+
+                    gameDic[ichat.Topic].BadAttack(msg.Sender.Handle);
+                    return "";
+                }
+
+                if (strs.Length > 2)
+                {
+                    gameDic[ichat.Topic].Attack(msg.Sender.Handle, target.Handle, strs[2]);
+                }
+                else
+                {
+                    gameDic[ichat.Topic].Attack(msg.Sender.Handle, target.Handle);
+                }
+            }
+
+            return "";
+        }
+
+        private User findPlayer(string s, IChat ichat)
+        {
+            foreach(User user in ichat.Members){
+                if (user.Handle.Contains(s))
+                {
+                    return user;
+                }                
+            }
+
+            foreach (User user in ichat.Members)
+            {
+                if (user.DisplayName.Contains(s))
+                {
+                    return user;
+                }
+            }
+
+            foreach (User user in ichat.Members)
+            {
+                if (user.FullName.Contains(s))
+                {
+                    return user;
+                }
+            }
+
+            return null;
+        }
+
+        private String parseDice(String s)
+        {
+            s = s + " ";
+            List<int> results = new List<int>();
+            int state = 0;
+            int curr = 4; //"roll" length
+            char next;
+            String currnum = "";
+            int numdie = 0;
+            int negat = 1;
+
+            Random r = new Random();
+            bool sex = false;
+
+            String label = "";
+
+            while (curr < s.Length)
+            {
+                next = s[curr];
+                //richTextBox1.AppendText("staate " + state+ " next " + next);
+                switch (state)
+                {
+                    case 0:
+                        if (next >= '0' && next <= '9')
+                        {
+                            currnum = "" + next;
+                            numdie = 0;
+                            state = 1; //number
+                        }
+                        else if (next == ' ')
+                        {
+                        }
+                        else
+                        {
+                            throw new Exception();
+                        }
+                        break;
+                    case 1:
+                        if (next >= '0' && next <= '9')
+                        {
+                            currnum += next;
+                            state = 1; //number
+                        }
+                        else if (next == 'd')
+                        {
+                            numdie = Int32.Parse(currnum);
+
+                            if (numdie == 69)
+                            {
+                                sex = true;
+                            }
+
+                            currnum = "";
+                            state = 2; //die size
+                        }
+                        else if (next == '+' || next == '-' || next == ' ')
+                        {
+                            int adds = Int32.Parse(currnum);
+                            if (adds == 69)
+                            {
+                                sex = true;
+                            }
+                            results.Add(negat * adds);
+
+                            currnum = "0";
+                            if (next == '+')
+                            {
+                                state = 0;
+                                negat = 1;
+                            }
+                            else if (next == '-')
+                            {
+                                state = 0;
+                                negat = -1;
+                            }
+                            else if (next == ' ')
+                            {
+                                state = 3; //op
+                            }
+                        }
+                        else
+                        {
+                            throw new Exception();
+                        }
+                        break;
+                    case 2:
+                        if (next >= '0' && next <= '9')
+                        {
+                            currnum += next;
+                            state = 2; 
+                        }
+                        else if (next == '+' || next == '-' || next == ' ')
+                        {
+                            int size = Int32.Parse(currnum);
+
+                            if (size == 69)
+                            {
+                                sex = true;
+                            }
+
+                            currnum = "0";
+                            for (int i = 0; i < numdie; ++i)
+                            {
+                                results.Add(negat * r.Next(1, size+1));
+                            }
+
+                            if (next == '+')
+                            {
+                                numdie = 0;
+                                state = 0;
+                                negat = 1;
+                            }
+                            else if (next == '-')
+                            {
+                                numdie = 0;
+                                state = 0;
+                                negat = -1;
+                            }
+                            else if (next == ' ')
+                            {
+                                numdie = 0;
+                                state = 3; //op
+                            }
+                        }
+                        else
+                        {
+                            throw new Exception();
+                        }
+                        break;
+
+                    case 3:
+                        if (next == '+')
+                        {
+                            state = 0;
+                            negat = 1;
+                        }
+                        else if (next == '-')
+                        {
+                            state = 0;
+                            negat = -1;
+                        }
+                        else if (next == ' ')
+                        { }
+                        else
+                        {
+                            label += next;
+                            state = 4;
+                        }
+                        break;
+                    case 4:
+                        label += next;
+                        break;
+                }
+
+                ++curr;
+            }
+
+
+            if (sex == true)
+            {
+                return "sex [sex]";
+            }
+
+            label = label.Trim();
+
+            int res = 0;
+            String outs = " [";
+
+            for (int i = 0; i < results.Count; ++i)
+            {
+                res += results[i];
+
+                if (results.Count < 100)
+                {
+                    outs += results[i];
+
+                    if (i < results.Count - 1)
+                    {
+                        outs += ", ";
+                    }
+                }
+            }
+
+            if (results.Count >= 100)
+            {
+                outs += "alot";
+            }
+
+            outs = res + outs + "]";
+
+            if(label.Length > 0)
+                outs += "(" + label + ")";
+
+            return outs;
+        }
+
+        private String RollDice(int num, int size)
+        {
+            String ret = "[";
+            int rum = 0;
+            Random r = new Random();
+            int temp;
+            for (int i = 0; i < num; i++)
+            {
+                temp = r.Next(1, size);
+                rum += temp;
+                ret += temp;
+
+                if (i < num - 1)
+                {
+                    ret += ", ";
+                }
+            }
+
+            ret += "]";
+
+            return rum + " "+ret;
+        }
+    }
+}
---
+++ SkypeBot/Game.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SKYPE4COMLib;
+
+namespace SkypeBot
+{
+    class Player
+    {
+        public int hp;
+        public int dmg;
+        public DateTime stun;
+
+        public Player()
+        {
+            hp = 20;
+            dmg = 5;
+            stun = DateTime.Now;
+        }
+    }
+
+    class Game
+    {
+        private Dictionary<string, Player> playerDic;
+        private Form1 form;
+        private IChat ichat;
+
+        public Game(Form1 f, IChat i)
+        {
+            playerDic = new Dictionary<string, Player>();
+            form = f;
+            ichat = i;
+        }
+
+        public void Attack(string p1, string p2, string special = "")
+        {
+            if (!playerDic.ContainsKey(p1))
+                playerDic[p1] = new Player();
+
+            if (!playerDic.ContainsKey(p2))
+                playerDic[p2] = new Player();
+
+            if (playerDic[p1].hp <= 0)
+            {
+                form.say(p1 + " tries to attack " + p2 + ", but is too dead to do that.", ichat, "FIGHT");
+            }
+            else if (playerDic[p1].stun.CompareTo(DateTime.Now) > 0)
+            {
+                form.say(p1 + " tries to attack " + p2 + ", but is stunned!", ichat, "FIGHT");
+            }
+            else
+            {
+
+                if (special == "boot")
+                {
+                    form.say(p1 + " boots " + p2 + "'s head! " + p2 + " is stunned for 5 seconds!", ichat, "FIGHT");
+                    playerDic[p2].stun = DateTime.Now.AddSeconds(5);
+                }
+                else
+                {
+
+                    playerDic[p2].hp -= playerDic[p1].dmg;
+
+                    form.say(p1 + " attacks " + p2 + " for " + playerDic[p1].dmg + "!", ichat, "FIGHT");
+                    form.say(p2 + "[" + playerDic[p2].hp + "/20]", ichat, "FIGHT");
+                }
+            }
+        }
+
+        public void BadAttack(string p)
+        {
+            form.say(p + " swings at air..?", ichat, "FIGHT");
+        }
+    }
+}
